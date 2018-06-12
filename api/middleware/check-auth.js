035ced47c05d671493f72c7a178c250cc62830f7
Dhruv <dhruv.acharya007@gmail.com>
2018-06-13 01:13:50 +0530

    authorization fix

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -3,7 +3,7 @@ require('./../../env');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization;
+        const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         
         req.userData = decoded;
