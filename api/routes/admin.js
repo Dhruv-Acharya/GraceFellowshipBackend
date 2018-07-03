36ab2456d8fb4b5a65611675bab89cbfb8cc6a0b
dhruvil75 <30633522+dhruvil75@users.noreply.github.com>
2018-07-03 10:51:18 +0530

    changes part 1 -> donation and volunteer corrected

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -10,13 +10,14 @@ require('./../../env');
 
 
 // insert volunteer
-router.post("/volunteer", function (req, res, next) {
+router.post("/campus/:campusId/volunteer", function (req, res, next) {
     class Volunteer {
         constructor(obj) {
             this.name = obj.name;
             this.username = obj.username;
             this.contact = obj.contact;
             this.email = obj.email;
+            this.campus_id = req.params.campusId
             this.password = bcrypt.hashSync(obj.password, 10);
             //this.password = bcrypt.hashSync(obj.password, process.env.BCRYPT_SALT_ROUNDS);
 
@@ -41,11 +42,13 @@ router.post("/volunteer", function (req, res, next) {
     });
 });
 
-// fetch all volunteers
-router.get("/volunteers", function (req, res, next) {
+// fetch all volunteers by campus Id
+router.get("/campus/:campusId/volunteers", function (req, res, next) {
     //console.log(req);
 
-    r.db('grace_fellowship').table('volunteer').without("password")
+    r.db('grace_fellowship').table('volunteer').filter({
+        campus_id:req.params.campusId
+    }).without("password")
         .run(req._dbconn, (err, vol) => {
 
             vol.toArray((err, result) => {
@@ -60,26 +63,38 @@ router.get("/volunteers", function (req, res, next) {
 });
 
 // get a single volunteer by ID
-router.get("/volunteer/:id", function (req, res, next) {
+router.get("/campus/:campusId/volunteer/:id", function (req, res, next) {
 // console.log(req.params.id);
 
     var volunteerId = req.params.id;
-    r.db('grace_fellowship').table('volunteer').get(volunteerId).without('password')
+    r.db('grace_fellowship').table('volunteer').filter({
+        campus_id : req.params.campusId,
+        id : req.params.id
+    }).without('password')
         .run(req._dbconn, (err, vol) => {
             // console.log(err);
             
-            if (vol)
-                res.status(200).json(vol);
-            else
-                res.status(404).json("Volunteer Not Found");
+            
+                vol.toArray((err,result)=>{
+                    if(result[0]){
+                        res.status(200).json(result[0]);
+                    }
+                    else
+                        res.status(404).json("Volunteer Not Found");
+                });
+                
+            
         });
 });
 
 // delete a single volunteer
-router.delete("/volunteer/:id", function (req, res, next) {
+router.delete("/campus/:campusId/volunteer/:id", function (req, res, next) {
 
     var volunteerId = req.params.id;
-    r.db('grace_fellowship').table('volunteer').get(volunteerId).delete()
+    r.db('grace_fellowship').table('volunteer').filter({
+        campus_id : req.params.campusId,
+        id : req.params.id
+    }).delete()
         .run(req._dbconn, (err, status) => {
             if (status.deleted > 0)
                 res.status(200).json(status.deleted);
@@ -88,33 +103,36 @@ router.delete("/volunteer/:id", function (req, res, next) {
         });
 });
 
-// delete an array of volunteers
-router.delete("/volunteers", function (req, res, next) {
+// // delete an array of volunteers
+// router.delete("/volunteers", function (req, res, next) {
 
-    var deleted = new Array();
-    var notDeleted = new Array();
-    req.body.volunteersId.forEach(volunteerId => {
-        r.db('grace_fellowship').table('volunteer').get(volunteerId).delete()
-            .run(req._dbconn, (err, status) => {
-                if (status.deleted > 0)
-                    deleted.push(volunteerId);
-                else
-                    notDeleted.push(volunteerId);
+//     var deleted = new Array();
+//     var notDeleted = new Array();
+//     req.body.volunteersId.forEach(volunteerId => {
+//         r.db('grace_fellowship').table('volunteer').get(volunteerId).delete()
+//             .run(req._dbconn, (err, status) => {
+//                 if (status.deleted > 0)
+//                     deleted.push(volunteerId);
+//                 else
+//                     notDeleted.push(volunteerId);
 
-                res.status(200).json({ deleted, notDeleted });
-            });
-    });
-});
+//                 res.status(200).json({ deleted, notDeleted });
+//             });
+//     });
+// });
 
 // update single volunteer by ID
-router.patch("/volunteer/:id", function (req, res, next) {
+router.patch("/campus/:campusId/volunteer/:id", function (req, res, next) {
     var volunteerId = req.params.id;
 
     if (req.body.password) {
         req.body.password = bcrypt.hashSync(req.body.password, 10);
     }
 
-    r.db('grace_fellowship').table('volunteer').get(volunteerId).update(req.body)
+    r.db('grace_fellowship').table('volunteer').filter({
+        campus_id : req.params.campusId,
+        id : volunteerId
+    }).update(req.body)
         .run(req._dbconn, (err, result) => {
             if (result.replaced > 0)
                 res.status(200).send(result.replaced + "");
@@ -201,6 +219,7 @@ router.post("/campus/:campusId/batch_member", function (req, res, next) {
             this.contact = obj.contact;
             this.address = obj.address;
             this.gender = obj.gender;
+            this.join_date = obj.joinDate;
         }
     }
 
@@ -388,11 +407,11 @@ router.patch('/campus/:campusId/instrument/:instrumentId',function(req, res, nex
 
 //Adding Donation via campus ID
 
-router.post('/campus/:campusId/donation_category',function (req, res, next){
+router.post('/donation_category',function (req, res, next){
 
     class donationCategory{
         constructor(obj){
-            this.campus_id = req.params.campusId;
+            
             this.category = obj.category;
         }
     }
@@ -407,36 +426,29 @@ router.post('/campus/:campusId/donation_category',function (req, res, next){
     });
 });
 
-//fetching donation category of a single campus by ID
+//fetching a single donation category by ID
 
-router.get('/campus/:campusId/donation_category',function (req, res, next){
+router.get('/donation_category/:id',function (req, res, next){
 
-    r.db('grace_fellowship').table('donation_category').filter({
-        campus_id:req.params.campusId
-    }).without('campus_id').run(req._dbconn,(err ,result)=>{
+    r.db('grace_fellowship').table('donation_category').get(req.params.id).run(req._dbconn,(err ,result)=>{
         if(err){
             res.status(500).json(err);
         }
         else{
-            result.toArray((err, donation_category) => {
-                if (err) {
-                    res.status(500).json(err);
-                }
-                else {
-                    res.status(200).json(donation_category);
-                }
-            });
+            if (!result) {
+                res.status(500).json("No Category Found");
+            }
+            else {
+                res.status(200).json(result);
+            }
         }
     });
 });
 
-//deleting donation category of a campus by campus Id and donation Id
-router.delete('/campus/:campusId/donation_category/:donation_categoryId',function(req, res, next){
+//deleting donation category by  donation Id
+router.delete('/donation_category/:donation_categoryId',function(req, res, next){
     
-    r.db('grace_fellowship').table('donation_category').filter({
-        "id":req.params.donation_categoryId,
-        "campus_id":req.params.campusId
-    }).delete().run(req._dbconn, (err, result)=>{
+    r.db('grace_fellowship').table('donation_category').get(req.params.donation_categoryId).delete().run(req._dbconn, (err, result)=>{
         if(err){
             res.status(500).json(err);
         }
@@ -446,13 +458,10 @@ router.delete('/campus/:campusId/donation_category/:donation_categoryId',functio
     });
 });
 
-//updating instrument of a campus by campus Id and  Instrument Id
-router.patch('/campus/:campusId/donation_category/:donation_categoryId',function(req, res, next){
+//updating donation by  donation category Id
+router.patch('/donation_category/:donation_categoryId',function(req, res, next){
     
-    r.db('grace_fellowship').table('donation_category').filter({
-        "id":req.params.donation_categoryId,
-        "campus_id":req.params.campusId
-    }).update({
+    r.db('grace_fellowship').table('donation_category').get(req.params.donation_categoryId).update({
         "category":req.body.category
     }).run(req._dbconn, (err, result)=>{
         if(err){
